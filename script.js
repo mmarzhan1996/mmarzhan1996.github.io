0e0efddb0e303236bd82249d7c78fd60a258d056
mmarzhan1996 <46340766+mmarzhan1996@users.noreply.github.com>
2019-03-06 16:03:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 $(function(){
   var navBtn = $('.header-btn');
-  var mobNav = $('.mobile-nav');
+
 
   navBtn.on('click', function(){
     mobNav.slideToggle();
   });
-  mobNav.on('click', function(){
-    mobNav.slideUp();
-  });
+
 
   // слайдер
   var slider = $('.slider');
