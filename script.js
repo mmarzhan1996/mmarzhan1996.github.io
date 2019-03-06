d5808d6e73e16eff55a044ecd17c649483dbce0f
mmarzhan1996 <46340766+mmarzhan1996@users.noreply.github.com>
2019-03-06 15:59:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+$(function(){
+  var navBtn = $('.header-btn');
+  var mobNav = $('.mobile-nav');
+
+  navBtn.on('click', function(){
+    mobNav.slideToggle();
+  });
+  mobNav.on('click', function(){
+    mobNav.slideUp();
+  });
+
+  // слайдер
+  var slider = $('.slider');
+  var sliderImg = $('.item');
+  slider.owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        }
+    },
+    onInitialized: function(){
+      equalHeight(sliderImg);
+    }
+
+  });
+// выравнивание всех картинок по меньшей высоте
+  function equalHeight($items){
+    var max = 0;
+
+    $items.each(function(){
+      var h = $(this).height();
+      if(h >= max){
+        max = h;
+      }
+    });
+    $items.css('height', max + 'px');
+  }
+
+});
