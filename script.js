c3b6ed1fc38ee19be7096cfd469aa196b6a2f900
mmarzhan1996 <46340766+mmarzhan1996@users.noreply.github.com>
2019-03-07 12:10:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+$(function(){
+  var navBtn = $('.header-btn');
+  var mobNav = $('.mobile-nav');
+
+  navBtn.on('click', function(){
+    mobNav.slideToggle();
+  });
+
+
+  // слайдер
+  var slider = $('.slider');
+  var sliderImg = $('.item');
+  slider.owlCarousel({
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    responsive:{
+        0:{
+            items:1
+        }
+    },
+    onInitialized: function(){
+      equalHeight(sliderImg);
+    }
+
+  });
+// выравнивание всех картинок по меньшей высоте
+  function equalHeight($items){
+    var max = 0;
+
+    $items.each(function(){
+      var h = $(this).height();
+      if(h >= max){
+        max = h;
+      }
+    });
+    $items.css('height', max + 'px');
+  }
+// анимация картинок в новостях
+  var newsImg = $('.first-news-photo');
+
+  newsImg.hover(function(){
+    newsImg.css('border', '4px solid orange');
+  },function(){
+    newsImg.css('border', '4px solid white');
+  });
+
+
+
+
+
+
+
+});
